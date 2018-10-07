8bfb6fa9d3c0205f02ec781a56f3f7939c59d901
Gregor Ahcin <bubi.grega7@gmail.com>
2018-10-07 21:51:25 +0200

    added emit and listen

--- server/server.js
+++ server/server.js
@@ -18,6 +18,16 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("New user connected.");
 
+  socket.emit("newMessage", {
+    from: "test@test.com",
+    text: "New message is in.",
+    createdAt: 11111111
+  });
+
+  socket.on("createMessage", message => {
+    console.log("New message created", message);
+  });
+
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
