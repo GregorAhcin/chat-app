2d5cf29feb42c9ee15ff4f3bd6ad2002e3c41e52
Gregor Ahcin <bubi.grega7@gmail.com>
2018-10-10 11:06:43 +0200

    added user joined

--- server/server.js
+++ server/server.js
@@ -17,14 +17,32 @@ app.use(express.static(publicPath));
 
 io.on("connection", socket => {
   console.log("New user connected.");
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome to chat app.",
+    joinedAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user joined.",
+    joinedAt: new Date().getTime()
+  });
 
   socket.on("createMessage", message => {
     console.log("New message created", message);
     io.emit("newMessage", {
+      // posljes vsem tudi sebi
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit("newMessage", {
+    //   // Posljes vsem razen sebi
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on("disconnect", () => {
